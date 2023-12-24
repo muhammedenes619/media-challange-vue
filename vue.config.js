2e040b566581a4957972194311e6747bf50d14a8
Muhammed Enes KAMARI <anas.kamari.619@gmail.com>
2023-12-24 19:56:40 +0300

    test

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true,
 });
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/media-challange-vue/" : "/",
+  publicPath: "",
 };
